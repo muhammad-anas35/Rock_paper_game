edf4a30d677e9da33aa93fdad7326ad1be03e533
M Anas <m401anas@gmail.com>
2025-02-05 04:23:54 +0500

    Update the code of Game

--- Game.py
+++ Game.py
@@ -8,8 +8,31 @@ class RPS(Enum):
     SCISSORS = 3
 
 
-choice = input("Enter... \n1: for Rock \n 2:for Paper \n 3:for Scissors\n\n ")
+choice = input("Enter... \n 1: for Rock \n 2:for Paper \n 3:for Scissors\n\n ")
 player = int(choice) 
+
+Pc_choice = random.choice("123")
+computer = int(Pc_choice)
+
 if player < 1 or player > 3:
     sys.exit("Chose between 1,2 and 3 only ")
-# print("You chose : " + str(RPS(player)).replace('RPS.',''))
+
+print("")
+print("You chose : " + str(RPS(player)).replace('RPS.',''))
+print("You chose : " + str(RPS(computer)).replace('RPS.',''))
+print("")
+
+
+
+if player == 1 and computer == 3:
+    print("You won ")
+elif player == 3 and computer == 2:
+    print("You won ")
+elif player == 2 and computer == 3:
+    print("You won ")
+elif player == computer:
+    print("Match Tie ")
+else:
+    print("Python Win")
+
+
